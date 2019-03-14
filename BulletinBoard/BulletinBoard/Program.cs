572a03506cfe4a7e565ff4eb09fc94d2e9a496f4
cjda <carljohan.danling@gmail.com>
2019-03-14 10:31:17 +0100

    AppDbContext, ShowMenu, Main.

--- BulletinBoard/BulletinBoard/Program.cs
+++ BulletinBoard/BulletinBoard/Program.cs
@@ -8,10 +8,120 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulletinBoard
 {
-    class Program
+    public class AppDbContext : DbContext
     {
+        //public DbSet<Category> Category { get; set; }
+        //public DbSet<Product> Product { get; set; }
+        //public DbSet<OrderItem> OrderItem { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=*************;Integrated Security=True");
+        }
+    }
+
+    public class Program
+    {
+        static AppDbContext database;
+
         static void Main(string[] args)
         {
+            using (database = new AppDbContext())
+            {
+                while (true)
+                {
+                    string option = ShowMenu("What do you want to do?", new[] {
+                        "Start shopping",
+                        "Quit"
+                    });
+
+                    if (option == "Start shopping") ListCategories();
+                    else Environment.Exit(0);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static string ShowMenu(string prompt, string[] options)
+        {
+            Console.WriteLine(prompt);
+
+            int selected = 0;
+
+            // Hide the cursor that will blink after calling ReadKey.
+            Console.CursorVisible = false;
+
+            ConsoleKey? key = null;
+            while (key != ConsoleKey.Enter)
+            {
+                // If this is not the first iteration, move the cursor to the first line of the menu.
+                if (key != null)
+                {
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = Console.CursorTop - options.Length;
+                }
+
+                // Print all the options, highlighting the selected one.
+                for (int i = 0; i < options.Length; i++)
+                {
+                    var option = options[i];
+                    if (i == selected)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine("- " + option);
+                    Console.ResetColor();
+                }
+
+                // Read another key and adjust the selected value before looping to repeat all of this.
+                key = Console.ReadKey().Key;
+                if (key == ConsoleKey.DownArrow)
+                {
+                    selected = Math.Min(selected + 1, options.Length - 1);
+                }
+                else if (key == ConsoleKey.UpArrow)
+                {
+                    selected = Math.Max(selected - 1, 0);
+                }
+            }
+
+            // Reset the cursor and perform the selected action.
+            Console.CursorVisible = true;
+            Console.Clear();
+            return options[selected];
+        }
+
+        static void WriteUnderlined(string line)
+        {
+            Console.WriteLine(line);
+            Console.WriteLine(new String('-', line.Length));
+        }
+
+        static string ReadString(string prompt)
+        {
+            Console.Write(prompt + ": ");
+            return Console.ReadLine();
+        }
+
+        static int ReadInt(string prompt)
+        {
+            Console.Write(prompt + ": ");
+            int? number = null;
+            while (number == null)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    number = int.Parse(input);
+                }
+                catch
+                {
+                    Console.Write("Please enter a valid integer: ");
+                }
+            }
+            return (int)number;
         }
     }
 }
