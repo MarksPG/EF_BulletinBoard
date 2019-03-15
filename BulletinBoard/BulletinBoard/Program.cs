2f14c99c41cab01985f23a3ff4e62fa227e3aef2
Per G <pgg.kinna@gmail.com>
2019-03-15 10:58:39 +0100

    Almost finished CreateAPost method. Can't save to database form some reason.

--- BulletinBoard/BulletinBoard/Program.cs
+++ BulletinBoard/BulletinBoard/Program.cs
@@ -179,16 +179,19 @@ namespace BulletinBoard
             Console.Clear();
             WriteUnderlined("Create new post");
             Console.WriteLine();
-            string[] categories = database.Category.Select(c => c.Name).ToArray();
-            string selectedCategory = ShowMenu("Select category", categories);
+            Category[] categories = database.Category.ToArray();
+            object selectedCategory = ShowMenu2("Select category", categories);
             Post post = new Post();
             //Behöver kollas. Det är ingen referens till objektet. ShowMenu behöver returnera ett objekt.
-            post.Category.Name = selectedCategory;
+            post.Category = (Category)selectedCategory;
             Console.WriteLine();
             post.Topic = ReadString("Enter a topic for your post");
             post.Content = ReadString("Write your post message");
             post.User = loggedInUser;
             post.Date = DateTime.Now;
+
+            database.Add(post);
+            database.SaveChanges();
         }
 
         private static void CreateAccount()
@@ -250,6 +253,56 @@ namespace BulletinBoard
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                     Console.WriteLine("- " + option);
+                    Console.ResetColor();
+                }
+
+                // Read another key and adjust the selected value before looping to repeat all of this.
+                key = Console.ReadKey().Key;
+                if (key == ConsoleKey.DownArrow)
+                {
+                    selected = Math.Min(selected + 1, options.Length - 1);
+                }
+                else if (key == ConsoleKey.UpArrow)
+                {
+                    selected = Math.Max(selected - 1, 0);
+                }
+            }
+
+            // Reset the cursor and perform the selected action.
+            Console.CursorVisible = true;
+            Console.Clear();
+            return options[selected];
+        }
+
+        static Category ShowMenu2(string prompt, Category[] options)
+        {
+            Console.WriteLine(prompt);
+
+            int selected = 0;
+
+            // Hide the cursor that will blink after calling ReadKey.
+            Console.CursorVisible = false;
+
+            ConsoleKey? key = null;
+            while (key != ConsoleKey.Enter)
+            {
+                // If this is not the first iteration, move the cursor to the first line of the menu.
+                if (key != null)
+                {
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = Console.CursorTop - options.Length;
+                }
+
+                // Print all the options, highlighting the selected one.
+                for (int i = 0; i < options.Length; i++)
+                {
+                    var option = options[i];
+                    if (i == selected)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine("- " + option.Name);
                     Console.ResetColor();
                 }
 
